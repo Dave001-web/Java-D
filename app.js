dc47472e531f1e2342e55e5985c14b6d75c2cda8
Dave001-web <davidshun991@gmail.com>
2025-03-24 15:37:27 +0100

    js intro

--- app.js
+++ app.js
@@ -1,88 +1,96 @@
-try {
-    console.log(age);
-    //do smth
-} catch (error) {
-    //error.message = "sth broke"
-    // handle the error
-    // error message
-    console.log(error.message);
-}
+// // Document object Model (DOM)
 
-// optional chaining
-const person = {
-    name: "bob",
-    age: 76,
-}
-console.log(person.job);// will not break code
-console.log(person.job);//
-let error = {}
+// // select element on the document, tagName(h1, p), className. idNaame, combination
+
+// const headings = document.getElementsByTagName("h1")
+// console.log(headings);
+
+// const myH1s = document.getElementsByClassName("heading")
+// console.log(myH1s);
+
+// const textPara = document.getElementById("text")
+// console.log(textPara);
+
+// // querySelector(css selector) one 
+// // ., #, div p, div .inner
+// const firstPara = document.querySelector("p")
+// console.log(firstPara);
+
+// const allParas = document.querySelectorAll("p")
+// console.log(allParas);
+
+// // querySelectorAll
+
+// // interacting with the contents on the doc
+// // textContent, innerText, innerHTML
+
+const heading = document.querySelector(".heading")
+console.log(heading.textContent);
+console.log(heading.innerText);
+heading.textContent += "JS IS FUN"
+//heading.textContent = "Js is FUN"
+
+const container = document.querySelector("div")
+console.log(container.innerHTML);
+container.innerHTML += "<a href='https://google.com'> visit google</a> "
+
+const myName = "Azeez Thiago"
+const intro = document.querySelector("h2")
+intro.textContent = `Welcome ${myName}
+
+// interact with attributes`
+const mylink = document.querySelector(".mylink")
+
+// facebook
+mylink.textContent = "Facebook"
+mylink.setAttribute("href", "https://facebook.com");
+mylink.setAttribute("target", "_blank");
+mylink.getAttribute("href")
+console.log(mylink.getAttribute("href"));
+
+// interact with styles
+mylink.style.color = "red"
+mylink.style.textDecoration = "none"
+
 //
+const btn = document.querySelector("button")
+//btn.className = "mybtn"
+btn.classList.add("mybtn")
+btn.classList.add("kelvin")
 
-error?.firstName?.message
+// create element in js
+const section = document.createElement("section")
+section.innerHTML = "<h1>Created from JS </h1>"
+section.className = "mysection"
 
-// nullish coalescence
-// 
-const image = "car"
-const whatsappAvatar = image ?? "Avatar image"
-console.log(whatsappAvatar);
- 
+// append it body or whereever it is good
+const body = document.querySelector("body")
+
+body.appendChild(section)
+
+// responding to users intercation
+// 1. event e.g click, submit 2. event handler, function
+const testbtn = document.querySelector(".testbtn")
+
+testbtn.addEventListener("click", () => {
+    console.log("User clicked");
+    body.style.backgroundColor = "red"
+})
 
 
-let balance = 5000
-let amount = 3000
-let pin = 1234
-let attempts = 3
+// form handling
+// submit
+// click
+const form = document.querySelector("form")
+const fullname = document.querySelector(".fullname")
+const small = document.querySelector("form small")
 
-console.log(checkBalance());
-
-function withdrawAmount() {
-    if (amount <= balance) {
-        balance -= amount;
-        return `insufficient funds`
-    } else {
-        return `Transaction Successful`
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const fullnameValue = fullname.value.trim()
+    console.log("form submitted", fullnameValue);
+    if (fullnameValue === "") {
+        small.style.display = "block"
+        small.textContent = "Please provide a name"
     }
-}
-console.log(withdrawAmount());
-
-function depositAmount() {
-    balance += 3000
-    return `Deposit successful`
-}
-console.log(depositAmount());
-
-function checkPin(enteredPin) {
-    while(attempts > 0){
-        if (enteredPin === pin) {
-          return `Correct Pin`;
-        } else {
-          attempts--;
-          return `invalid pin, Try Again`;
-        }
-    }
-    return "Your Card has been Blocked"
-}
-function startAtm(choice) {
-    if(choice === 1){
-        // withdraw
-        withdrawAmount(1000)
-    } else if (choice === 2){
-        //deposit
-        depositAmount(3000)
-    }else if (choice === 3) {
-        //check balance
-        checkBalance()
-    }else if (choice === 4) {
-        // exit
-        return "Thank you for banking with us"
-    } else {
-        // invalid option
-        return "Invalid option, try again"
-    }
-}
-
-function checkBalance()  {
-return `Your current balance is ${balance}`
-}
-
-console.log(checkPin(4563));
+})
